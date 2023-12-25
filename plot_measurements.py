c0ed62b784f4b5a9a938b759371f36ebeb5220b4
Matthias Lüthi <maluethi@protonmail.ch>
2023-12-25 18:29:36 +0100

    moved from integrated energy to energy in the time interval

--- plot_measurements.py
+++ plot_measurements.py
@@ -27,15 +27,18 @@ fig, [ax_total, ax_power, ax_time] = plt.subplots(3,1, sharex=True)
 df_diff = df.diff()
 
 print(df.head())
-df.plot('Time', 'Lieferung', ax=ax_total, marker='x')
-df.plot('Time', 'Bezug', ax=ax_total, marker='x')
-ax_total.set_ylabel("Energy [kWh]")
+ax_total.step(df['Time'], df_diff["Lieferung"], marker='o', markersize=1)
+ax_total.step(df['Time'], df_diff["Bezug"], marker='o', markersize=1)
+
+#df.plot('Time', 'Lieferung', ax=ax_total, marker='x')
+#df.plot('Time', 'Bezug', ax=ax_total, marker='x')
+ax_total.set_ylabel("Energy per Interval [kWh]")
 
 power_out = df_diff["Lieferung"]*(3600) / (df_diff['Time'].dt.seconds)
 power_in = df_diff["Bezug"]*(3600) / (df_diff['Time'].dt.seconds)
 
-ax_power.plot(df['Time'], power_out, marker='o', markersize=1)
-ax_power.plot(df['Time'], power_in, marker='o', markersize=1)
+ax_power.step(df['Time'], power_out, marker='o', markersize=1)
+ax_power.step(df['Time'], power_in, marker='o', markersize=1)
 ax_power.set_ylabel("Power [kW]")
 
 ax_time.plot(df['Time'], df_diff['Time'].dt.seconds, marker='.', color='green')
