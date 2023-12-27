bf67b24c6ab79bfe7f94b61d5e4f0894b41a20e1
Andreas Lüthi <andreas.luethi@gmx.net>
2023-12-27 22:30:14 +0100

    ersparniss: Change tarife for 2024.

--- ersparniss.py
+++ ersparniss.py
@@ -25,8 +25,8 @@ file_path = 'data/2-weg-messpunkt.xlsx'  ## Bezung und Lieferung
 sheet_name = 'tab1'
 timesrie_lieferung, timeserie_bezug, timeserie_ts = get_timserie(file_path, sheet_name)
 
-tarif_bezung = 0.2905    # Tarif für Bezug in CHF/kWh 2024
-tarif_lieferung = 0.0824 # Tarif für Lieferung in CHF/kWh, 2023 mit HKN
+tarif_bezung = 0.3027    # Tarif für Bezug in CHF/kWh 2024 (https://www.strompreis.elcom.admin.ch/?category=H3)
+tarif_lieferung = 0.0824 # Tarif für Lieferung in CHF/kWh, 2023 (4.Q) mit HKN
 batterie_max_cap = 1.0   # Maximale Kapazität der Batterie in kWh
 ersparnisse = 0.0
 
