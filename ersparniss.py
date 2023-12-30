92283453a26a49301bf34c61faeaa03ee07718c4
Andreas Lüthi <andreas.luethi@gmx.net>
2023-12-30 10:30:55 +0100

    ersparniss: count also remaining battery capacity

--- ersparniss.py
+++ ersparniss.py
@@ -18,27 +18,27 @@ def berechne_ersparniss(timeserie_bezug, timeserie_lieferung, tarif_bezug, tarif
             batterie_cur_cap -= min(bezug, batterie_cur_cap)
             ersparniss += (lc - batterie_cur_cap) * tarif_bezug
 
-    return ersparniss
+    return ersparniss, batterie_cur_cap * tarif_bezug
 
 
 file_path = 'data/2-weg-messpunkt.xlsx'  ## Bezung und Lieferung
 sheet_name = 'tab1'
 timesrie_lieferung, timeserie_bezug, timeserie_ts = get_timserie(file_path, sheet_name)
 
-tarif_bezung = 0.3027    # Tarif für Bezug in CHF/kWh 2024 (https://www.strompreis.elcom.admin.ch/?category=H3)
-tarif_lieferung = 0.0824 # Tarif für Lieferung in CHF/kWh, 2023 (4.Q) mit HKN
-batterie_max_cap = 1.0   # Maximale Kapazität der Batterie in kWh
+tarif_bezung = 0.3027  # Tarif für Bezug in CHF/kWh 2024 (https://www.strompreis.elcom.admin.ch/?category=H3)
+tarif_lieferung = 0.0824  # Tarif für Lieferung in CHF/kWh, 2023 (4.Q) mit HKN
+batterie_max_cap = 1.0  # Maximale Kapazität der Batterie in kWh
 ersparnisse = 0.0
 
 # Ersparnisse berechnen
 # todo -- Consider charging and discharging losses
 while True:
-    opt_ersparnisse = berechne_ersparniss(timesrie_lieferung, timeserie_bezug, tarif_bezung, tarif_lieferung,
-                                          batterie_max_cap)
-    if opt_ersparnisse > ersparnisse:
-    ###if batterie_max_cap < 1500:
-        ersparnisse = opt_ersparnisse
-        print(f"Ersparnis: {opt_ersparnisse :.2f} CHF mit {batterie_max_cap:.0f} kWh Batterie")
+    opt_ersparnisse, rest = berechne_ersparniss(timesrie_lieferung, timeserie_bezug, tarif_bezung, tarif_lieferung,
+                                                batterie_max_cap)
+    total_ersparnis = opt_ersparnisse + rest
+    if total_ersparnis > ersparnisse:
+        ersparnisse = total_ersparnis
+        print(f"Total Ersparnis: {total_ersparnis:.2f} / Rest {rest:.2f} CHF mit {batterie_max_cap:.0f} kWh Batterie")
         batterie_max_cap += 1.0
     else:
         break
